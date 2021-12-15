c4ef4355c1b58d2b23a58b196b48d9bb330068de
Jim97M <wabuyajames@gmail.com>
2021-12-15 07:10:14 +0300

    Models

---
+++ src/main/java/com/devmanager/DevmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.devmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmanager/DevmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
